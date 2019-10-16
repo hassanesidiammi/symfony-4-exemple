9fe64a7c85c26540f12d4e545d04d178fae59bdf
Hassane <hsidiammi@gmail.com>
2019-10-16 18:18:27 +0100

    Debug toolbar, composer req Profiler.

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,10 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    KnpU\LoremIpsumBundle\KnpULoremIpsumBundle::class => ['all' => true],
 ];
